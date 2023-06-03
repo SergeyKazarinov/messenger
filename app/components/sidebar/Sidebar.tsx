36e79e4117e8cd2c6dd329b85d8fd032095f43da
Sergey Kazarinov <kazarinov092@gmail.com>
2023-06-03 13:38:54 +0500

    feat(actions): add geCurrentUser

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,16 +1,19 @@
+import getCurrentUser from '@/app/actions/getCurrentUser';
 import DesktopSidebar from './DesktopSidebar';
 import MobileFooter from './MobileFooter';
 
-const Sidebar = async ({ children }: { children: React.ReactNode }) => (
+const Sidebar = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
+  return (
 
-  <div className="h-full">
-    <DesktopSidebar />
-    <MobileFooter />
-    <main className="lg:pl-20 h-full">
-
-      {children}
-    </main>
-  </div>
-);
+    <div className="h-full">
+      <DesktopSidebar />
+      <MobileFooter />
+      <main className="lg:pl-20 h-full">
+        {children}
+      </main>
+    </div>
+  );
+};
 
 export default Sidebar;
