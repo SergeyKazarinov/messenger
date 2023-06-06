9ea6c80040d27d51bd782161edafe79d401d4a97
Sergey Kazarinov <kazarinov092@gmail.com>
2023-06-06 18:59:29 +0500

    fix(eslint): fix eslint errors
    off rules: no-vars vars-on-top
    fix user page (delete handleClick)
    remove console.log in the Select components

--- app/components/input/Select.tsx
+++ app/components/input/Select.tsx
@@ -13,34 +13,31 @@ interface SelectProps {
 
 const Select: FC<SelectProps> = ({
   label, value, onChange, options, disabled,
-}) => {
-  console.log('asd');
-  return (
-    <div className="z-[100]">
-      <label className="block text-sm font-medium leading-6 text-gray-900">
-        {label}
-      </label>
-      <div className='mt-2'>
-        <ReactSelect
-          isDisabled={disabled}
-          value={value}
-          onChange={onChange}
-          isMulti
-          options={options}
-          menuPortalTarget={document.body}
-          styles={{
-            menuPortal: (base) => ({
-              ...base,
-              zIndex: 9999,
-            }),
-          }}
-          classNames={{
-            control: () => 'text-sm',
-          }}
-        />
-      </div>
+}) => (
+  <div className="z-[100]">
+    <label className="block text-sm font-medium leading-6 text-gray-900">
+      {label}
+    </label>
+    <div className='mt-2'>
+      <ReactSelect
+        isDisabled={disabled}
+        value={value}
+        onChange={onChange}
+        isMulti
+        options={options}
+        menuPortalTarget={document.body}
+        styles={{
+          menuPortal: (base) => ({
+            ...base,
+            zIndex: 9999,
+          }),
+        }}
+        classNames={{
+          control: () => 'text-sm',
+        }}
+      />
     </div>
-  );
-};
+  </div>
+);
 
 export default Select;
